c28965b1971a1d682edafa96a236c16ce168de40
gregory6z <gregoryrag@gmail.com>
2024-05-21 11:38:59 +0200

    :heavy_check_mark: feat  : initial structure

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vitest/config"
+import tsConfigPaths from "vite-tsconfig-paths"
+
+
+export default defineConfig({
+  test: {
+    globals: true,
+    root: "./",
+  },
+  plugins: [
+    tsConfigPaths(),
+    
+  ],
+})
+
+
